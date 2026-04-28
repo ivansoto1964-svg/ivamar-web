fbc5f9dd0fdee1b9b1afe4cb9578d0d19c34e75a
Ivan Soto <ivansoto@MacBook-Pro-de-Ivan.local>
2026-04-28 13:30:10 -0400

    Fix typo in terms-es export

--- src/views/terms-es.js
+++ src/views/terms-es.js
@@ -1,4 +1,4 @@
-kmodule.exports = `
+module.exports = `
 <style>
 @import url('https://fonts.googleapis.com/css2?family=Syne:wght@400;600;700;800&family=JetBrains+Mono:wght@300;400&display=swap');
 .legal-page{font-family:'Syne',sans-serif;background:#030508;color:#F0F4FF;margin:-20px;overflow-x:hidden;}
