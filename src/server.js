0bb31dddf7cec9ae886843e42c403acbd7cf14c1
Ivan Soto <TU_EMAIL_AQUI>
2026-07-14 16:26:48 -0400

    Redirect Caribex /sitemap.xml to /caribex-sitemap.xml

--- src/server.js
+++ src/server.js
@@ -2014,11 +2014,7 @@ app.get("/sitemap.xml", async (req, res) => {
   // Route to correct sitemap based on host
   const host = req.hostname;
   if (host.includes('yourcaribbeanexpert')) {
-    const base = "https://www.yourcaribbeanexpert.com";
-    const destinations = ['puerto-rico','dominican-republic','cuba','jamaica','grand-cayman','haiti','vieques-culebra','barbados','santa-lucia','trinidad-tobago','sint-maarten','martinique','guadeloupe','st-barths','grenada','antigua','dominica','st-kitts-nevis','aruba','curacao','bonaire','usvi','bvi','turks-caicos','bahamas','tulum','cartagena','san-andres','costa-rica','belize','panama','roatan','venezuela','corn-islands','guatemala-caribbean'];
-    const urls = [`<url><loc>${base}/</loc><changefreq>weekly</changefreq><priority>1.0</priority></url>`,`<url><loc>${base}/caribex</loc><changefreq>weekly</changefreq><priority>0.9</priority></url>`,`<url><loc>${base}/about</loc><changefreq>monthly</changefreq><priority>0.7</priority></url>`,`<url><loc>${base}/privacidad</loc><changefreq>monthly</changefreq><priority>0.5</priority></url>`,`<url><loc>${base}/terminos</loc><changefreq>monthly</changefreq><priority>0.5</priority></url>`,...destinations.map(s=>`<url><loc>${base}/caribex/${s}</loc><changefreq>monthly</changefreq><priority>0.8</priority></url>`)].join('');
-    res.set('Content-Type','application/xml');
-    return res.send(`<?xml version="1.0" encoding="UTF-8"?><urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">${urls}</urlset>`);
+    return res.redirect(301, '/caribex-sitemap.xml');
   }
   // Get posts from Blogger RSS directly
   let postUrls = '';
